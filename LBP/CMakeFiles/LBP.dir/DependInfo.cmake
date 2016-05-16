88c7df8a40c79ffce094e25f686046ab6881911f
Kevin Hitzler <kevin.hitzler@yahoo.com>
2016-05-16 11:59:26 +0200

    LBP Test

---
+++ LBP/CMakeFiles/LBP.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kevin/ComputerVision/LBP/LBP.cpp" "/home/kevin/ComputerVision/LBP/CMakeFiles/LBP.dir/LBP.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/kevin/opencv-2.4.9"
+  "/home/kevin/opencv-2.4.9/include"
+  "/home/kevin/opencv-2.4.9/include/opencv"
+  "/home/kevin/opencv-2.4.9/modules/core/include"
+  "/home/kevin/opencv-2.4.9/modules/flann/include"
+  "/home/kevin/opencv-2.4.9/modules/imgproc/include"
+  "/home/kevin/opencv-2.4.9/modules/highgui/include"
+  "/home/kevin/opencv-2.4.9/modules/features2d/include"
+  "/home/kevin/opencv-2.4.9/modules/calib3d/include"
+  "/home/kevin/opencv-2.4.9/modules/ml/include"
+  "/home/kevin/opencv-2.4.9/modules/video/include"
+  "/home/kevin/opencv-2.4.9/modules/legacy/include"
+  "/home/kevin/opencv-2.4.9/modules/objdetect/include"
+  "/home/kevin/opencv-2.4.9/modules/photo/include"
+  "/home/kevin/opencv-2.4.9/modules/gpu/include"
+  "/home/kevin/opencv-2.4.9/modules/ocl/include"
+  "/home/kevin/opencv-2.4.9/modules/nonfree/include"
+  "/home/kevin/opencv-2.4.9/modules/contrib/include"
+  "/home/kevin/opencv-2.4.9/modules/stitching/include"
+  "/home/kevin/opencv-2.4.9/modules/superres/include"
+  "/home/kevin/opencv-2.4.9/modules/ts/include"
+  "/home/kevin/opencv-2.4.9/modules/videostab/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ LBP/CMakeFiles/LBP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LBP.dir/LBP.cpp.o"
+  "LBP.pdb"
+  "LBP"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LBP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
