fffa8a673dd129581e70be24078d1809ab1ab7e0
Kevin Hitzler <kevin.hitzler@yahoo.com>
2016-05-17 10:32:01 +0200

    changed main

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,38 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+
 #include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-int main(char *argv[])
+struct dirent *readdir(DIR *dirp);
+struct stat sb;
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Path" << argv << endl;
+    // check the number of parameters
+    if (argc < 2)
+    {
+        cerr << "Usage: " << argv[0] << " <DirectoyOfTrainingImages>" << endl;
+        return 1;
+    }
 
-    waitKey(0);
+    // print the directory
+    cout << "Searching for \"" << argv[1] << "\" ..." << endl;
+
+    // search for directory
+
+    if(stat(argv[1], &sb) != 0 || !S_ISDIR(sb.st_mode))
+    {
+        cerr << "Error: \"" << argv[1] <<"\" is not a valid directory." << endl;
+        return 1;
+    }
+
+    cout << "Directory " << argv[1] << " was found, " << sb.st_nlink << " links:" << endl;
     return 0;
-
 }
