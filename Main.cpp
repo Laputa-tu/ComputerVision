9fd77b00526a4e2faf11d28abf7f5aa2bf68cac5
Kevin Hitzler <kevin.hitzler@yahoo.com>
2016-05-17 14:44:54 +0200

    read in directories recursively

--- Main.cpp
+++ Main.cpp
@@ -2,16 +2,52 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-#include <iostream>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdio.h>
 #include <dirent.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 using namespace cv;
 
-struct dirent *readdir(DIR *dirp);
-struct stat sb;
+
+void printFilesInDirectory(char *path, int depth = 1)
+{
+    DIR *dirp = opendir(path);
+    struct dirent *dp;
+    struct stat sb;
+
+    if(dirp)
+    {
+        while ((dp = readdir(dirp)) != NULL)
+        {
+            // create new path
+            char *newpath = (char *) malloc(2 + strlen(path) + strlen(dp->d_name));
+            strcpy(newpath, path);
+            strcat(newpath, "/");
+            strcat(newpath, dp->d_name);
+
+            // skip self and parent
+            if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
+                continue;
+
+            if(stat(newpath, &sb) == 0 && S_ISDIR(sb.st_mode))
+                printFilesInDirectory(newpath, depth+1);
+            else
+            {
+                cout << "\t";
+                for(int i=0; i<depth; i++) cout << " - ";
+                cout << path << "/" << dp->d_name << endl;
+            }
+
+
+        }
+    }
+
+    closedir(dirp);
+}
 
 int main(int argc, char* argv[])
 {
@@ -26,13 +62,15 @@ int main(int argc, char* argv[])
     cout << "Searching for \"" << argv[1] << "\" ..." << endl;
 
     // search for directory
-
+    struct stat sb;
     if(stat(argv[1], &sb) != 0 || !S_ISDIR(sb.st_mode))
     {
         cerr << "Error: \"" << argv[1] <<"\" is not a valid directory." << endl;
         return 1;
     }
 
-    cout << "Directory " << argv[1] << " was found, " << sb.st_nlink << " links:" << endl;
+    // print files
+    printFilesInDirectory(argv[1]);
+
     return 0;
 }
