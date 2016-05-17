67aea6ede47c82f60ce070dfcfe9af3d6fe87679
Kevin Hitzler <kevin.hitzler@yahoo.com>
2016-05-17 20:02:22 +0200

    Get Files from recursive search

--- Main.cpp
+++ Main.cpp
@@ -12,45 +12,13 @@
 using namespace std;
 using namespace cv;
 
+//definitions
+#define SLOTH_ZEBRA "zebra.json"
+#define SLOTH_SIGN "sign.json"
 
-void searchSlothFilesInDirectory(char *path, char *name, int depth = 1)
-{
-    DIR *dirp = opendir(path);
-    struct dirent *dp;
-    struct stat sb;
-
-    if(dirp)
-    {
-        while ((dp = readdir(dirp)) != NULL)
-        {
-            // create new path
-            char *newpath = (char *) malloc(2 + strlen(path) + strlen(dp->d_name));
-            strcpy(newpath, path);
-            strcat(newpath, "/");
-            strcat(newpath, dp->d_name);
-
-            // skip self and parent
-            if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
-                continue;
-
-            if(stat(newpath, &sb) == 0 && S_ISDIR(sb.st_mode))
-                searchSlothFilesInDirectory(newpath, name, depth+1);
-            else
-            {
-                cout << "\t";
-                for(int i=0; i<depth; i++) cout << " =";
-                cout << "> " << path << "/" << dp->d_name;
-
-                if(strcmp(dp->d_name, name) == 0)
-                    cout << "\t\t<=== Found JSON File!" << endl;
-                else
-                    cout << endl;
-            }
-        }
-    }
-
-    closedir(dirp);
-}
+//functions
+void getFilesInDirectory(char *, const char*, int, vector<string> *);
+void getFilesInDirectory(char *, const char*, vector<string> *);
 
 int main(int argc, char* argv[])
 {
@@ -72,9 +40,61 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // print files
-    char sloth[100] = "sloth.json";
-    searchSlothFilesInDirectory(argv[1], sloth);
+    // search & get files
+    vector<string> files[100];
+    getFilesInDirectory(argv[1], SLOTH_ZEBRA, files);
+
+    cout << "Found the following JSON files: " << endl;
+    for(vector<string>::iterator it = files->begin(); it != files->end(); ++it)
+        cout << "\tFile: " << *it << endl;
 
     return 0;
+}
+
+void getFilesInDirectory(char *path, const char *name, vector<string> *files)
+{
+    return getFilesInDirectory(path,name,1, files);
+}
+
+void getFilesInDirectory(char *path, const char *name, int depth, vector<string> *files)
+{
+    DIR *dirp = opendir(path);
+    struct dirent *dp;
+    struct stat sb;
+
+    if(dirp)
+    {
+        while ((dp = readdir(dirp)) != NULL)
+        {
+            // create new path
+            char *newpath = (char *) malloc(2 + strlen(path) + strlen(dp->d_name));
+            strcpy(newpath, path);
+            strcat(newpath, "/");
+            strcat(newpath, dp->d_name);
+
+            // skip self and parent
+            if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
+                continue;
+
+            if(stat(newpath, &sb) == 0 && S_ISDIR(sb.st_mode))
+                getFilesInDirectory(newpath, name, depth+1, files);
+            else
+            {
+                cout << "\t";
+                for(int i=0; i<depth; i++) cout << " =";
+                cout << "> " << path << "/" << dp->d_name;
+
+                if(strcmp(dp->d_name, name) == 0)
+                {
+                    cout << "\t\t<=== Found JSON File!" << endl;
+                    string file_path(newpath);
+                    files->push_back(file_path);
+                }
+                else
+                    cout << endl;
+            }
+        }
+    }
+
+    closedir(dirp);
 }
