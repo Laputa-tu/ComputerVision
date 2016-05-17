35c24ca7942f3e86d4ebc2588fea78ceafa06cdc
Kevin Hitzler <kevin.hitzler@yahoo.com>
2016-05-18 01:34:58 +0200

    kevin finished

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,12 @@
+#ifdef _MSC_VER
+#include <boost/config/compiler/visualc.hpp>
+#endif
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
+#include <cassert>
+#include <exception>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -19,6 +28,98 @@ using namespace cv;
 //functions
 void getFilesInDirectory(char *, const char*, int, vector<string> *);
 void getFilesInDirectory(char *, const char*, vector<string> *);
+vector<double> StringToVector(string);
+
+class JSONImage
+{
+    private:
+        string name, path;
+        vector<double> xn;
+        vector<double> yn;
+    public:
+        JSONImage(void)
+        {
+
+        }
+
+        ~JSONImage(void)
+        {
+
+        }
+
+        void setXn(string xn_string)
+        {
+            this->xn = StringToVector(xn_string);
+        }
+
+        void setYn(string yn_string)
+        {
+            this->yn = StringToVector(yn_string);
+        }
+
+        vector<double> getXn()
+        {
+            return this->xn;
+        }
+
+        vector<double> getYn()
+        {
+            return this->yn;
+        }
+
+        void printXn()
+        {
+            for(int i=0; i<xn.size(); i++)
+            {
+                cout.precision(17);
+                cout << xn.at(i) << "; ";
+            }
+
+            cout << endl;
+        }
+
+        void printYn()
+        {
+            for(int i=0; i<yn.size(); i++)
+            {
+                cout.precision(17);
+                cout << yn.at(i) << "; ";
+            }
+
+            cout << endl;
+        }
+
+        void setName(string name)
+        {
+            this->name = name;
+        }
+        string getName()
+        {
+            return this->name;
+        }
+
+        void setPath(string path)
+        {
+            this->path = path;
+        }
+        string getPath()
+        {
+            return this->path;
+        }
+
+        bool hasPolygon()
+        {
+            if(xn.size() > 0 && yn.size() > 0)
+                return true;
+            else
+                return false;
+        }
+};
+
+// more functions
+vector<JSONImage> getJSONImages(vector<string> *);
+
+
 
 int main(int argc, char* argv[])
 {
@@ -46,9 +147,152 @@ int main(int argc, char* argv[])
 
     cout << "Found the following JSON files: " << endl;
     for(vector<string>::iterator it = files->begin(); it != files->end(); ++it)
+    {
         cout << "\tFile: " << *it << endl;
+    }
 
+    // get images from json
+    vector<JSONImage> json_images = getJSONImages(files);
+    cout << "Found " << json_images.size() << " labled images in json files:" << endl;
+    for(int i=0; i<json_images.size(); i++)
+    {
+        cout << "\tImage: "<<json_images.at(i).getName() << endl;
+    }
+
+    // read image
+    Mat image, rescaled;
+    image = imread(json_images.at(0).getPath(), CV_LOAD_IMAGE_COLOR);
+
+    if(! image.data ) // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+
+
+    int scale_n_times = 3;
+    rescaled = image;
+    for(int i=0; i<scale_n_times; i++)
+    {
+        resize(rescaled, rescaled, Size(), 0.5, 0.5, INTER_CUBIC);
+        cout << "Width: " << rescaled.cols << endl;
+        cout << "Height: " << rescaled.rows << endl;
+        imshow( "Rescaled", rescaled );
+
+        // build sliding window
+        int windows_n_rows = 128;
+        int windows_n_cols = 64;
+        int step_slide_row = windows_n_rows/3;
+        int step_slide_col = windows_n_cols/3;
+
+        for(int row = 0; row <= rescaled.rows - windows_n_rows; row += step_slide_row)
+        {
+            for(int col = 0; col <= rescaled.cols - windows_n_cols; col += step_slide_col )
+            {
+                Rect windows(col, row, windows_n_cols, windows_n_rows);
+                Mat Roi = rescaled(windows);
+
+                cout << "Width: " << Roi.cols << endl;
+                cout << "Height: " << Roi.rows << endl;
+
+                namedWindow( "Sliding Window", WINDOW_AUTOSIZE );
+                imshow( "Sliding Window", Roi );
+                waitKey(0);
+            }
+        }
+    }
+
+    waitKey(0);
     return 0;
+}
+
+/*
+ *
+ * void Classifier::train(const cv::Mat3b& img, ClipperLib::Path labelPolygon, cv::Rect slidingWindow)
+{
+    /*
+    ClipperLib::Path labelPolygon;
+    labelPolygon << IntPoint(0, 0) << IntPoint(70, 0) << IntPoint(100, 60) << IntPoint(70, 100) << IntPoint(0, 50);
+    //slidingWindow << IntPoint(20, 20) << IntPoint(120, 20) << IntPoint(120, 80) << IntPoint(20, 80);
+    cv::Rect slidingWindow = cv::Rect(0, 0, 64, 128);
+    */
+
+
+vector<JSONImage> getJSONImages(vector<string> *files)
+{
+    vector<JSONImage> imageList;
+
+    // collect images for each json file
+    for(vector<string>::iterator it = files->begin(); it != files->end(); ++it)
+    {
+        // get directory
+        string file_path = *it;
+        int index = file_path.find(SLOTH_ZEBRA);
+        string file_directory = file_path.substr(0, index);
+
+        try
+        {
+            boost::property_tree::ptree pt;
+            boost::property_tree::read_json(*it, pt);
+
+            BOOST_FOREACH(boost::property_tree::ptree::value_type& v, pt)
+            {
+                JSONImage currentImage;
+                BOOST_FOREACH(boost::property_tree::ptree::value_type& i, v.second)
+                {
+                    // get and print name
+                    string name = i.second.get_value("filename");
+                    currentImage.setName(name);
+                    currentImage.setPath(file_directory + name);
+
+                    // get labeled polygons
+                    BOOST_FOREACH(boost::property_tree::ptree::value_type& j, i.second)
+                    {
+                        BOOST_FOREACH(boost::property_tree::ptree::value_type& k, j.second)
+                        {
+                            string xn = k.second.get_value("xn");
+                            string yn = k.second.get_value("yn");
+
+                            if(!xn.empty() && !yn.empty())
+                            {
+                                currentImage.setXn(xn);
+                                currentImage.setYn(yn);
+                            }
+                        }
+                    }
+                }
+
+                if(currentImage.hasPolygon())
+                {
+                    imageList.push_back(currentImage);
+                }
+            }
+        }
+        catch (std::exception const& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    return imageList;
+}
+
+vector<double> StringToVector(string str)
+{
+    vector<double> vect;
+    stringstream ss(str);
+    double i;
+
+    while (ss >> i)
+    {
+        vect.push_back(i);
+
+        if (ss.peek() == ';')
+            ss.ignore();
+    }
+
+    return vect;
 }
 
 void getFilesInDirectory(char *path, const char *name, vector<string> *files)
