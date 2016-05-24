33df0df1bc8836636b0793d8470a760606686438
hoebd <danielhoeb@yahoo.de>
2016-05-24 07:35:43 +0200

    SVM Threshold

--- HOG/classifier.cc
+++ HOG/classifier.cc
@@ -102,7 +102,7 @@ double Classifier::classify(const cv::Mat3b& img, cv::Rect slidingWindow, float 
 
 	//predict Result
 	double result = -svm.predict(descriptor, true);
-	cout << "Result:              " << result << endl;
+	//cout << "Result:              " << result << endl;
 	
 	if(result > 0) //svm prediction: -1 to +1
 	{
