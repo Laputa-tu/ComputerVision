53f4c9b269585a030b1387a04dcacde930c18315
Kevin Hitzler <kevin.hitzler@yahoo.com>
2016-06-19 21:32:33 +0200

    added functionality to ROC.m

--- ROC/ROC.m
+++ ROC/ROC.m
@@ -4,6 +4,19 @@ full_c = strcat(path_c, file_end);
 files_c = dir(full_c);
 
 C_labels = [];
+label_c_length = 0;
+
+% get label length (largest label)
+for file_c = files_c'
+    c_name = file_c.name;
+    c_label = regexprep(c_name, '_', ' ');
+    c_label = regexprep(c_label, '.csv', '');
+    c_length = length(c_label);
+    
+    if(c_length > label_c_length)
+        label_c_length = c_length;
+    end
+end
 
 % get matrices of classifier roc
 for file_c = files_c'
@@ -12,6 +25,12 @@ for file_c = files_c'
     c_label = regexprep(c_name, '_', ' ');
     c_label = regexprep(c_label, '.csv', '');
     c_path = strcat(path_c, c_name);
+    c_length = length(c_label);
+    
+    while label_c_length > c_length
+        c_label = sprintf('%s ',c_label);
+        c_length = length(c_label);
+    end
     
     % read data
     C = dlmread(c_path, '\t');
@@ -38,6 +57,19 @@ full_d = strcat(path_d, file_end);
 files_d = dir(full_d);
 
 D_labels = [];
+label_d_length = 0;
+
+% get label length (largest label)
+for file_d = files_d'
+    d_name = file_d.name;
+    d_label = regexprep(d_name, '_', ' ');
+    d_label = regexprep(d_label, '.csv', '');
+    d_length = length(d_label);
+    
+    if(d_length > label_d_length)
+        label_d_length = d_length;
+    end
+end
 
 % get matrices of classifier roc
 for file_d = files_d'    
@@ -46,6 +78,12 @@ for file_d = files_d'
     d_path = strcat(path_d, d_name);
     d_label = regexprep(d_name, '_', ' ');
     d_label = regexprep(d_label, '.csv', '');
+    d_length = length(d_label);
+    
+    while label_d_length > d_length
+        d_label = sprintf('%s ',d_label);
+        d_length = length(d_label);
+    end
     
     % read data
     D = dlmread(d_path, '\t');
@@ -63,5 +101,6 @@ xlabel('False positive rate');
 ylabel('True positive rate');
 title('ROC for Detection by Clustering');
 legend(D_labels);
-hold off;        
+hold off;
+        
 
